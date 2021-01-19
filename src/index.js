df77fd4bfd23118c3e8e5fc27c30354bea24755f
SantosItgam <sntvicencio@gmail.com>
2021-01-19 07:47:29 -0600

    Matarialize Working Testing with colors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 /* eslint-disable no-console */
 // Script de Frontend
 // importar los estilos
-import './styles/styles.css';
+// import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
 
-console.log('Modern JS Working!!! Thanks Babel...');
+// console.log('Modern JS Working!!! Thanks Babel...');
 // const show = (message = 'Saludo por defecto') => {
 //  alert(message);
 // };
