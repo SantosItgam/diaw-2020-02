c93e1047bc548a956c4be3d53515a901fed601ad
SantosItgam <sntvicencio@gmail.com>
2021-01-19 08:51:27 -0600

    Avance Dropdown

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
 // import './styles/styles.css';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize';
+import initDropdown from './helpers/dropdowninit';
 
+initDropdown();
 // console.log('Modern JS Working!!! Thanks Babel...');
 // const show = (message = 'Saludo por defecto') => {
 //  alert(message);
