5327129f030ebf1c0f8587bbdab585c3cb0192d4
SantosItgam <sntvicencio@gmail.com>
2021-01-21 20:59:01 -0600

    Tailwind

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
-// Script de Frontend
-// importar los estilos
-// import './styles/styles.css';
-import 'materialize-css/dist/css/materialize.css';
-import 'materialize-css/dist/js/materialize';
-import initDropdown from './helpers/dropdown';
-
-initDropdown.init();
-// console.log('Modern JS Working!!! Thanks Babel...');
-// const show = (message = 'Saludo por defecto') => {
-//  alert(message);
-// };
+import './styles/styles.css';
